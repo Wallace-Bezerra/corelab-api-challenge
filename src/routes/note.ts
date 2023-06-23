c87f59aca4e3a0625696a1ee7f41fc9db32ba8cf
Wallace-Bezerra <89711908+Wallace-Bezerra@users.noreply.github.com>
2023-06-23 19:59:49 -0300

    fix: script vercel

--- src/routes/note.ts
+++ src/routes/note.ts
@@ -1,84 +1,82 @@
 import { FastifyInstance } from "fastify";
 import { prisma } from "../lib/prisma";
-import z from "zod"
+import z from "zod";
 export async function NoteRoutes(app: FastifyInstance) {
-  
-
-  app.get("/notes", async(req, res) => {
+  app.get("/notes", async (req, res) => {
     const notes = await prisma.note.findMany({
       orderBy: {
-        createdAt: "desc"
-      }
-    })
-    res.send(notes)
-  })
+        createdAt: "desc",
+      },
+    });
+    res.send(notes);
+  });
 
   app.post("/notes", async (req, res) => {
     const paramsSchemaBody = z.object({
       title: z.string(),
       content: z.string(),
       isFavorite: z.coerce.boolean().default(false),
-      color: z.string().default("#ffffff")
-    })
-    const result = paramsSchemaBody.safeParse(req.body)
+      color: z.string().default("#ffffff"),
+    });
+    const result = paramsSchemaBody.safeParse(req.body);
     if (!result.success) {
-      res.send(result.error.message)
+      res.send(result.error.message);
     } else {
-      const { color, content, isFavorite, title } = result.data
+      const { color, content, isFavorite, title } = result.data;
       const note = await prisma.note.create({
-        data:
-        {
+        data: {
           title,
           content,
           color,
           isFavorite,
-        }
-      })
+        },
+      });
       res.send(note);
     }
-  })
-  
+  });
 
   app.delete("/notes/:id", async (req, res) => {
     const paramsSchemaId = z.object({
-      id: z.string()
-    })
-    const result = paramsSchemaId.safeParse(req.params)
+      id: z.string(),
+    });
+    const result = paramsSchemaId.safeParse(req.params);
     if (!result.success) {
-      res.send(result.error)
+      res.send(result.error);
     } else {
-      const { id } = result.data
+      const { id } = result.data;
       await prisma.note.delete({
         where: {
-          id: id
-        }
-      })
+          id,
+        },
+      });
     }
-    res.send("Deleted")
-  })
-  app.patch("/notes/:id", async(req, res) => {
+    res.send("Deleted");
+  });
+  app.patch("/notes/:id", async (req, res) => {
     const paramsSchemaId = z.object({
-      id: z.string()
-    })
-    const { id } = paramsSchemaId.parse(req.params)
+      id: z.string(),
+    });
+    const { id } = paramsSchemaId.parse(req.params);
     const paramsSchemaBody = z.object({
       title: z.string(),
       content: z.string(),
       isFavorite: z.coerce.boolean().default(false),
-      color: z.string().default("#ffffff")
-    })
-    const { title, content, color, isFavorite } = paramsSchemaBody.parse(req.body)
+      color: z.string().default("#ffffff"),
+    });
+    const { title, content, color, isFavorite } = paramsSchemaBody.parse(
+      req.body
+    );
     const editNote = await prisma.note.update({
       where: {
-        id: id,
+        id,
       },
       data: {
         title,
         content,
         color,
         isFavorite,
-      }
-    })
-    res.send(editNote)
-  })
+      },
+    });
+    res.send(editNote);
+  });
 }
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import fastify from "fastify";
 import { NoteRoutes } from "./routes/note";
 
-const app = fastify()
+const app = fastify();
 
-app.register(NoteRoutes)
+app.register(NoteRoutes);
 
-app.listen({
-  port: 3333,
-}).then(() => {
-  console.log("Server Runnnig on http://localhost:3333")
-})
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log("Server Runnnig on http://localhost:3333");
+  });
