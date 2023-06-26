fcede9c248fea39d7e6cae9780df3f4fd4bcc2c4
Wallace-Bezerra <89711908+Wallace-Bezerra@users.noreply.github.com>
2023-06-26 13:42:01 -0300

    fix: ajuste banco de dados

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import fastify from "fastify";
+import cors from "@fastify/cors";
 import { NoteRoutes } from "./routes/note";
 
 const app = fastify();
 
+app.register(cors, { origin: true });
 app.register(NoteRoutes);
 
 app
